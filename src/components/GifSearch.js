dca8e3ca775bcb726c85a3af242ff40da7c04286
Ann Duong <30392423+thuyanduong@users.noreply.github.com>
2022-10-24 02:04:54 -0400

    Update GifSearch.js

--- src/components/GifSearch.js
+++ src/components/GifSearch.js
@@ -3,7 +3,7 @@ function GifSearch(){
         <div>
             <label htmlFor="searchInput">Enter a Search Term </label>
             <input type="text" className="form-control" id="searchInput" />
-            <button type="button" class="btn btn-success">Search</button>
+            <button type="button" className="btn btn-success">Search</button>
         </div>
     )
 }
