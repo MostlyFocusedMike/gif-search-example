3468e83d0de7f5ae8ff5123dd386a6242cb8c325
benspector-mls <ben@marcylabschool.org>
2023-05-09 13:05:37 -0400

    removed react import

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-
-function NavBar(props){
+function NavBar(props) {
   const colors = {
     black: 'navbar-inverse',
     white: 'navbar-default'
   }
-  
+
   return (
     <nav className={`navbar ${colors[props.color]}`}>
       <div className='container-fluid'>
         <div className='navbar-header'>
           <div className='navbar-brand'>
-            { props.title }
+            {props.title}
           </div>
         </div>
       </div>
